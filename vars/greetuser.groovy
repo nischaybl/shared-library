75eac26e5305b78d3a973452d31b8608e6542303
nischaybl <87368770+nischaybl@users.noreply.github.com>
2021-07-21 18:30:16 +0530

    Create greetuser.groovy

---
+++ vars/greetuser.groovy
@@ -0,0 +1,15 @@
+def call (String username = 'null')
+echo "This is a shared library greetuser: ${username}"
+pipeline {
+  environment {
+    USER_NAME="$(username)"
+    }
+   agent any
+    stages{
+      stage{
+        steps{
+          echo "Hi $USER_NAME, How are you! "
+       } 
+     }
+   }
+ }
